366a5ff18d547331a13fafe03b6eef0fecd4ff3a
Viktor <viktor@farcic.com>
2017-08-11 17:21:26 +0200

    Docker Image Build

--- vars/dockerDeploy.groovy
+++ vars/dockerDeploy.groovy
@@ -1,8 +1,8 @@
-def call(String project, String hubUser) {
+def call(String project, String hubUser, String hostIp, String basePath) {
     script {
         try {
             sh "docker service update --image ${hubUser}/${project}:${env.BUILD_NUMBER} ${project}_main"
-            sh "TAG=${env.BUILD_NUMBER} docker-compose -p ${project}-${env.BUILD_NUMBER} run --rm production"
+            sh "TAG=${env.BUILD_NUMBER} HOST_IP=${hostIp} SERVICE_PATH=${basePath}-${env.BUILD_NUMBER} docker-compose -p ${project}-${env.BUILD_NUMBER} run --rm production"
         } catch (e) {
             sh "docker service update --rollback ${project}_main"
             error "Failed to update the service"
--- vars/dockerFunctional.groovy
+++ vars/dockerFunctional.groovy
@@ -1,6 +1,6 @@
-def call(String project, String hubUser) {
-    sh "TAG=beta-${env.BUILD_NUMBER} docker stack deploy -c stack-test.yml ${project}-beta-${env.BUILD_NUMBER}"
+def call(String project, String hubUser, String hostIp, String basePath) {
+    sh "TAG=beta-${env.BUILD_NUMBER} SERVICE_PATH=${basePath}-${env.BUILD_NUMBER} docker stack deploy -c stack-test.yml ${project}-beta-${env.BUILD_NUMBER}"
     sh "docker image build -f Dockerfile.test -t ${hubUser}/${project}-test:${env.BUILD_NUMBER} ."
     sh "docker image push ${hubUser}/${project}-test:${env.BUILD_NUMBER}"
-    sh "TAG=${env.BUILD_NUMBER} docker-compose -p ${project}-${env.BUILD_NUMBER} run --rm functional"
+    sh "TAG=${env.BUILD_NUMBER} HOST_IP=${hostIp} SERVICE_PATH=${basePath}-${env.BUILD_NUMBER} docker-compose -p ${project}-${env.BUILD_NUMBER} run --rm functional"
 }
