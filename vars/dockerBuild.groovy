6c703b8a558bd2d79ec85e79738c2fb72b96e319
Viktor <viktor@farcic.com>
2017-08-11 16:12:00 +0200

    Docker Image Build

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,13 +1,11 @@
 def call(String project, String hubUser) {
-    steps {
-        sh "docker image build -t ${hubUser}/${project}:beta-${env.BUILD_NUMBER} ."
-        withCredentials([usernamePassword(
-                credentialsId: "docker",
-                usernameVariable: "USER",
-                passwordVariable: "PASS"
-        )]) {
-            sh "docker login -u $USER -p $PASS"
-        }
-        sh "docker image push ${hubUser}/${project}:beta-${env.BUILD_NUMBER}"
+    sh "docker image build -t ${hubUser}/${project}:beta-${env.BUILD_NUMBER} ."
+    withCredentials([usernamePassword(
+            credentialsId: "docker",
+            usernameVariable: "USER",
+            passwordVariable: "PASS"
+    )]) {
+        sh "docker login -u $USER -p $PASS"
     }
+    sh "docker image push ${hubUser}/${project}:beta-${env.BUILD_NUMBER}"
 }
