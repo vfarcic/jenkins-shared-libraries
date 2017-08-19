fea6afe8ffa2454afb6447052ad8bbf8763c2ac2
Viktor <viktor@farcic.com>
2017-08-19 18:57:22 +0200

    Multibranch

--- vars/dockerFunctional.groovy
+++ vars/dockerFunctional.groovy
@@ -2,6 +2,5 @@ def call(String project, String hubUser, String hostIp, String basePath) {
     sh "TAG=beta-${env.BUILD_NUMBER} SERVICE_PATH=${basePath}-${env.BUILD_NUMBER} docker stack deploy -c stack-test.yml ${project}-beta-${env.BUILD_NUMBER}"
     sh "docker image build -f Dockerfile.test -t ${hubUser}/${project}-test:${env.BUILD_NUMBER} ."
     sh "docker image push ${hubUser}/${project}-test:${env.BUILD_NUMBER}"
-    sleep 10
     sh "TAG=${env.BUILD_NUMBER} HOST_IP=${hostIp} SERVICE_PATH=${basePath}-${env.BUILD_NUMBER} docker-compose -p ${project}-${env.BUILD_NUMBER} run --rm functional"
 }
