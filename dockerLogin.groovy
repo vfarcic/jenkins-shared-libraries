0ee8f8277286b89d6844491165a9fff0abebfe6e
Viktor <viktor@farcic.com>
2017-09-24 23:09:34 +0200

    DF vars

---
+++ dockerLogin.groovy
@@ -0,0 +1,9 @@
+def call() {
+    withCredentials([usernamePassword(
+            credentialsId: "docker",
+            usernameVariable: "USER",
+            passwordVariable: "PASS"
+    )]) {
+        sh "docker login -u $USER -p $PASS"
+    }
+}
--- vars/dfBuild.groovy
+++ vars/dfBuild.groovy
@@ -1,6 +1,7 @@
 def call(String project) {
     sh "docker image build -t vfarcic/${project} ."
     sh "docker image build -t vfarcic/${project}-docs -f Dockerfile.docs ."
-    sh "docker tag vfarcic/docker-flow-proxy vfarcic/${project}:${currentBuild.displayName}"
-    sh "docker tag vfarcic/docker-flow-proxy-docs vfarcic/${project}-docs:${currentBuild.displayName}"
+    sh "docker tag vfarcic/${project} vfarcic/${project}:${currentBuild.displayName}"
+    sh "docker tag vfarcic/${project}-docs vfarcic/${project}-docs:${currentBuild.displayName}"
+    sh "docker tag vfarcic/${project} vfarcic/docker-flow-proxy:beta"
 }
