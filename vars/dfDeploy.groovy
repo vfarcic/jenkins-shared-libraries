9a34d8d91922690358f4ed5a155766fbb0c69bf7
Viktor <viktor@farcic.com>
2017-09-24 23:47:57 +0200

    DF vars

--- vars/dfDeploy.groovy
+++ vars/dfDeploy.groovy
@@ -1,4 +1,8 @@
 def call(String serviceName, String docsServiceName) {
     sh "docker service update --image vfarcic/docker-flow-proxy:${currentBuild.displayName} ${serviceName}"
-    sh "docker service update --image vfarcic/docker-flow-proxy-docs:${currentBuild.displayName} ${docsServiceName}"
+    script {
+        if (docsServiceName != "") {
+            sh "docker service update --image vfarcic/docker-flow-proxy-docs:${currentBuild.displayName} ${docsServiceName}"
+        }
+    }
 }
