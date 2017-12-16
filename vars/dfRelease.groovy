6566743c7579a129f8770ba7d8aca1be2c7de249
Viktor <viktor@farcic.com>
2017-12-16 22:52:55 +0100

    Release

--- vars/dfRelease.groovy
+++ vars/dfRelease.groovy
@@ -1,8 +1,17 @@
-def call(String project) {
+def call(String project, gox = false) {
     dockerLogin()
-    sh "docker push vfarcic/${project}:latest"
-    sh "docker push vfarcic/${project}:${currentBuild.displayName}"
-    sh "docker push vfarcic/${project}-docs:latest"
-    sh "docker push vfarcic/${project}-docs:${currentBuild.displayName}"
+    sh "docker image push vfarcic/${project}:latest"
+    sh "docker image push vfarcic/${project}:${currentBuild.displayName}"
+    sh "docker image push vfarcic/${project}-docs:latest"
+    sh "docker image push vfarcic/${project}-docs:${currentBuild.displayName}"
     dockerLogout()
+    script {
+        if (gox) {
+            sh 'docker container run --rm -it -v $PWD:/src vfarcic/gox docker-flow-proxy'
+            withCredentials([usernamePassword(credentialsId: "github-token", variable: "GIHBUT_TOKEN")]) {
+                def msg = sh(returnStdout: true, script: "git log --format=%B -1").trim()
+                sh "docker container run --rm -it -e GITHUB_TOKEN=${GITHUB_TOKEN} -v ${pwd}:/src -w /src vfarcic/github-release git tag -a ${currentBuild.displayName} -m '${msg}'"
+            }
+        }
+    }
 }
