56aaac4de6c5dd34315907109bd6063a5789e881
Viktor <viktor@farcic.com>
2017-12-17 02:03:00 +0100

    Release testing

--- vars/dfReleaseGithub.groovy
+++ vars/dfReleaseGithub.groovy
@@ -1,5 +1,5 @@
 def call(String project) {
-    sh "docker container run --rm -v ${PWD}:/src vfarcic/gox docker-flow-proxy"
+    sh "docker container run --rm -v \${PWD}:/src vfarcic/gox docker-flow-proxy"
     withCredentials([usernamePassword(credentialsId: "github-token-2", usernameVariable: "USER", passwordVariable: "GITHUB_TOKEN")]) {
         script {
             def msg = sh(returnStdout: true, script: "git log --format=%B -1").trim()
@@ -14,15 +14,16 @@ def call(String project) {
                         releaseMsg = lines[i] + "\n"
                     }
                 }
-                sh "docker container run --rm -e GITHUB_TOKEN=${GITHUB_TOKEN} -v ${PWD}:/src -w /src vfarcic/github-release git tag -a ${currentBuild.displayName} -m '${releaseMsg}'"
-                sh "docker container run --rm -e GITHUB_TOKEN=${GITHUB_TOKEN} -v ${PWD}:/src -w /src vfarcic/github-release git push --tags"
-                sh "docker container run --rm -e GITHUB_TOKEN=${GITHUB_TOKEN} -v ${PWD}:/src -w /src vfarcic/github-release github-release release --user vfarcic --repo ${project} --tag ${currentBuild.displayName} --name '${releaseTitle}' --description '${releaseMsg}'"
+                def cmd = "docker container run --rm -e GITHUB_TOKEN=${GITHUB_TOKEN} -v \${PWD}:/src -w /src vfarcic/github-release"
+                sh "${cmd} git tag -a ${currentBuild.displayName} -m '${releaseMsg}'"
+                sh "${cmd} git push --tags"
+                sh "${cmd} github-release release --user vfarcic --repo ${project} --tag ${currentBuild.displayName} --name '${releaseTitle}' --description '${releaseMsg}'"
                 files = findFiles(glob: "${project}_*")
                 println files
                 println "---"
                 for (def file : files) {
                     println file
-                    sh "docker container run --rm -e GITHUB_TOKEN=${GITHUB_TOKEN} -v ${PWD}:/src -w /src vfarcic/github-release github-release upload --user vfarcic --repo ${project} --tag ${currentBuild.displayName} --name '${file.name}' --file ${file.name}"
+                    sh "${cmd} github-release upload --user vfarcic --repo ${project} --tag ${currentBuild.displayName} --name '${file.name}' --file ${file.name}"
                 }
             }
         }
