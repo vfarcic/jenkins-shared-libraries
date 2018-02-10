cce66bf0346cf079430aced056b7992d88557dd2
Viktor <viktor@farcic.com>
2018-02-10 21:57:11 +0100

    Changed to detached update

--- vars/dockerDeploy.groovy
+++ vars/dockerDeploy.groovy
@@ -1,7 +1,7 @@
 def call(String project, String hubUser, String hostIp, String basePath) {
     script {
         try {
-            sh "docker service update --image ${hubUser}/${project}:${env.BUILD_NUMBER} ${project}_main"
+            sh "docker service update -d --image ${hubUser}/${project}:${env.BUILD_NUMBER} ${project}_main"
             sh "TAG=beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER} HOST_IP=${hostIp} SERVICE_PATH=${basePath}-${env.BRANCH_NAME}-${env.BUILD_NUMBER} docker-compose -p ${project}-${env.BRANCH_NAME}-${env.BUILD_NUMBER} run --rm production"
         } catch (e) {
             sh "docker service update --rollback ${project}_main"
