c23c68644718751b8ad06b6191a67369a5f45bec
Raymond Mouthaan <raymondmmouthaan@gmail.com>
2018-04-01 15:54:01 +0200

    Update dfRelease.groovy

--- vars/dfRelease.groovy
+++ vars/dfRelease.groovy
@@ -4,15 +4,5 @@ def call(String project, gox = false) {
     sh "docker image push vfarcic/${project}:${currentBuild.displayName}"
     sh "docker image push vfarcic/${project}-docs:latest"
     sh "docker image push vfarcic/${project}-docs:${currentBuild.displayName}"
-    
-    sh """bash -c "
-        curl -L -o manifest-tool https://github.com/estesp/manifest-tool/releases/download/v0.7.0/manifest-tool-linux-amd64
-        chmod +x manifest-tool"
-    """
-    
-    sh """./manifest-tool push from-args --platforms linux/arm --template "vfarcic/${project}:${currentBuild.displayName}-OS-ARCH" --target "vfarcic/${project}:${currentBuild.displayName}""""
-    sh """./manifest-tool push from-args --platforms linux/arm --template "vfarcic/${project}:${currentBuild.displayName}-OS-ARCH" --target "vfarcic/${project}:latest""""
-    sh """./manifest-tool push from-args --platforms linux/arm --template "vfarcic/${project}:${currentBuild.displayName}-OS-ARCH" --target "vfarcic/${project}-docs:${currentBuild.displayName}""""
-    sh """./manifest-tool push from-args --platforms linux/arm --template "vfarcic/${project}:${currentBuild.displayName}-OS-ARCH" --target "vfarcic/${project}-docs:latest""""
     dockerLogout()
 }
