48fdf9c690d5744fce15094e87416d145b24f19c
Viktor <viktor@farcic.com>
2018-04-10 23:36:02 +0200

    Fix

--- vars/dfRelease2.groovy
+++ vars/dfRelease2.groovy
@@ -1,16 +1,16 @@
 def call(String project, gox = false) {
-    dfLogin()
-    sh "docker image push dockerflow/${project}:latest"
-    sh "docker image push dockerflow/${project}:${currentBuild.displayName}"
-    sh "docker image push dockerflow/${project}-docs:latest"
-    sh "docker image push dockerflow/${project}-docs:${currentBuild.displayName}"
-    dockerLogout()
-
     // dfLogin()
-    // Push image for linux-amd64
-    // sh "docker image push dockerflow/${project}:${currentBuild.displayName}-linux-amd64"
-    
+    // sh "docker image push dockerflow/${project}:latest"
+    // sh "docker image push dockerflow/${project}:${currentBuild.displayName}"
     // sh "docker image push dockerflow/${project}-docs:latest"
     // sh "docker image push dockerflow/${project}-docs:${currentBuild.displayName}"
     // dockerLogout()
+
+    dfLogin()
+    // Push image for linux-amd64
+    sh "docker image push dockerflow/${project}:${currentBuild.displayName}-linux-amd64"
+    
+    sh "docker image push dockerflow/${project}-docs:latest"
+    sh "docker image push dockerflow/${project}-docs:${currentBuild.displayName}"
+    dockerLogout()
 }
