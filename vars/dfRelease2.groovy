5a05a9119097641a611cd434907c06da96af88e1
Raymond Mouthaan <raymondmmouthaan@gmail.com>
2018-04-13 20:32:51 +0200

    Merged dfManifest2.groovy
    - Added manifest tool
    - Create and push manifest list
    - Added comments

--- vars/dfRelease2.groovy
+++ vars/dfRelease2.groovy
@@ -1,7 +1,8 @@
+/*******************************************
+ * Release multi-arch images to docker hub.
+ *******************************************/
 def call(String project, gox = false) {
-    /***************************************
-     * Release images to docker hub
-     ***************************************/
+
     dfLogin()
     
     // Push image for linux-amd64
@@ -15,6 +16,16 @@ def call(String project, gox = false) {
     
     // Push image for docs
     sh "docker image push dockerflow/${project}-docs:${currentBuild.displayName}"
+    
+    // Download manifest-tool
+    sh """bash -c "
+        curl -L -o manifest-tool https://github.com/estesp/manifest-tool/releases/download/v0.7.0/manifest-tool-linux-amd64
+        chmod +x manifest-tool"
+    """
+    
+    // Create and push manifest list
+    sh "./manifest-tool push from-args --platforms linux/amd64,linux/arm --template dockerflow/${project}:${currentBuild.displayName}-OS-ARCH --target dockerflow/${project}:${currentBuild.displayName}"
+    sh "./manifest-tool push from-args --platforms linux/amd64,linux/arm --template dockerflow/${project}:${currentBuild.displayName}-OS-ARCH --target dockerflow/${project}:latest"
     
     dockerLogout()
 }
