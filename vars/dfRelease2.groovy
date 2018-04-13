9828f5a3e82276ba43cb2763687129bcf77af592
Raymond Mouthaan <raymondmmouthaan@gmail.com>
2018-04-13 20:26:27 +0200

    Merged dfReleaseArm2.groovy
    - added push command for linux-arm
    - added comment

--- vars/dfRelease2.groovy
+++ vars/dfRelease2.groovy
@@ -1,9 +1,20 @@
 def call(String project, gox = false) {
+    /***************************************
+     * Release images to docker hub
+     ***************************************/
     dfLogin()
+    
     // Push image for linux-amd64
     sh "docker image push dockerflow/${project}:${currentBuild.displayName}-linux-amd64"
     
+    // Push image for linux-arm
+    sh "docker image push dockerflow/${project}:${currentBuild.displayName}-linux-arm"
+    
+    // Push image for docs latest
     sh "docker image push dockerflow/${project}-docs:latest"
+    
+    // Push image for docs
     sh "docker image push dockerflow/${project}-docs:${currentBuild.displayName}"
+    
     dockerLogout()
 }
