e64035ec419249211dfa5c51427da603366720ec
Raymond Mouthaan <raymondmmouthaan@gmail.com>
2018-05-17 11:05:45 +0200

    Replaced manifest-tool with docker manifest

--- vars/dfRelease2.groovy
+++ vars/dfRelease2.groovy
@@ -17,7 +17,7 @@ def call(String project, gox = false) {
     // Push image for docs
     sh "docker image push dockerflow/${project}-docs:${currentBuild.displayName}"
     
-    // Download manifest-tool
+    /* Download manifest-tool
     sh """bash -c "
         curl -L -o manifest-tool https://github.com/estesp/manifest-tool/releases/download/v0.7.0/manifest-tool-linux-amd64
         chmod +x manifest-tool"
@@ -26,6 +26,29 @@ def call(String project, gox = false) {
     // Create and push manifest list
     sh "./manifest-tool push from-args --platforms linux/amd64,linux/arm --template dockerflow/${project}:${currentBuild.displayName}-OS-ARCH --target dockerflow/${project}:${currentBuild.displayName}"
     sh "./manifest-tool push from-args --platforms linux/amd64,linux/arm --template dockerflow/${project}:${currentBuild.displayName}-OS-ARCH --target dockerflow/${project}:latest"
+    */
+  
+    /** Manifest Create Version **/
+    sh "docker manifest create dockerflow/${project}:${currentBuild.displayName} \
+        dockerflow/${project}:${currentBuild.displayName}-linux-amd64 \
+        dockerflow/${project}:${currentBuild.displayName}-linux-arm"
+
+    // Manifest Annotate Version
+    sh "docker manifest annotate dockerflow/${project}:${currentBuild.displayName} dockerflow/${project}:${currentBuild.displayName}-linux-arm --os=linux --arch=arm --variant=v7"
+  
+    // Manifest Push Version
+    sh "docker manifest push dockerflow/${project}:${currentBuild.displayName}"  
     
+    /** Manifest Create LATEST **/
+    sh "docker manifest create dockerflow/${project}:latest \
+        dockerflow/${project}:${currentBuild.displayName}-linux-amd64 \
+        dockerflow/${project}:${currentBuild.displayName}-linux-arm"
+
+    // Manifest Annotate LATEST
+    sh "docker manifest annotate dockerflow/${project}:latest dockerflow/${project}:${currentBuild.displayName}-linux-arm --os=linux --arch=arm --variant=v7"
+  
+    // Manifest Push LATEST
+    sh "docker manifest push dockerflow/${project}:latest"
+
     dockerLogout()
 }
