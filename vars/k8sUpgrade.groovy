70c6ce31668b5c3918a3ec496fb17876df573066
Viktor <viktor@farcic.com>
2018-07-10 15:24:25 +0200

    k8s

---
+++ vars/k8sUpgrade.groovy
@@ -0,0 +1,9 @@
+def call(project, addr) {
+    sh """helm upgrade \
+        ${project} \
+        helm/${project} -i \
+        --tiller-namespace ${project}-build \
+        --namespace ${project} \
+        --set image.tag=${currentBuild.displayName} \
+        --set ingress.host=${addr}"""
+}
