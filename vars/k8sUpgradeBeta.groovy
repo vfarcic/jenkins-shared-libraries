e55629daaf5f55c5127620175fbdeb20283e52cd
Viktor <viktor@farcic.com>
2018-07-18 01:14:26 +0200

    Extra values

--- vars/k8sUpgradeBeta.groovy
+++ vars/k8sUpgradeBeta.groovy
@@ -3,7 +3,7 @@ def call(project, domain, extraValues = "") {
     tagBeta = "${currentBuild.displayName}-${env.BRANCH_NAME}"
     addr = "${project}-${env.BUILD_NUMBER}-${env.BRANCH_NAME}.${domain}"
     sh """helm upgrade \
-        ${chartName} \
+        ${chartName.toLowerCase()} \
         helm/${project} -i \
         --tiller-namespace ${project}-build \
         --set image.tag=${tagBeta} \
