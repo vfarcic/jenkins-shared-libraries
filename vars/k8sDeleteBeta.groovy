ef0c265681815d70f4b6b2093df89a658b270575
Viktor <viktor@farcic.com>
2018-07-20 00:39:50 +0200

    k8s

--- vars/k8sDeleteBeta.groovy
+++ vars/k8sDeleteBeta.groovy
@@ -1,5 +1,5 @@
 def call(project) {
-    chartName = "${project}-${env.BUILD_NUMBER}-${env.BRANCH_NAME}"
+    chartName = "${project}-${env.BUILD_NUMBER}-${env.BRANCH_NAME.toLowerCase()}"
     sh """helm delete ${chartName} \
         --tiller-namespace ${project}-build \
         --purge"""
