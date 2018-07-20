b7c8267d6ad9474b896f15e2788abea78ed3dd9f
Viktor <viktor@farcic.com>
2018-07-20 23:23:43 +0200

    Jenkins CDP

--- vars/ocCreateEdgeRoute.groovy
+++ vars/ocCreateEdgeRoute.groovy
@@ -1,7 +1,9 @@
 def call(project, address) {
-    sh """oc -n ${project} \
-        create route edge \
-        --service ${project} \
-        --insecure-policy Allow \
-        --hostname ${address}"""
+    try { // Needs to be created only once
+        sh """oc -n ${project} \
+            create route edge \
+            --service ${project} \
+            --insecure-policy Allow \
+            --hostname ${address}"""
+    }
 }
