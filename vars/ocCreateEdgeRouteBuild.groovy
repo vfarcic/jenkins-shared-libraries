11a0f406b73e777f37c1324fe247d90837dabb1a
Viktor <viktor@farcic.com>
2018-07-20 23:05:24 +0200

    Jenkins CDP

--- vars/ocCreateEdgeRouteBuild.groovy
+++ vars/ocCreateEdgeRouteBuild.groovy
@@ -1,8 +1,5 @@
 def call(project, domain) {
-    echo "${project}"
-    echo "${domain}"
     chartName = "${project}-${env.BUILD_NUMBER}-${env.BRANCH_NAME}"
-    echo "${chartName}"
     addr = "${project}-${env.BUILD_NUMBER}-${env.BRANCH_NAME}.${domain}"
     sh """oc -n ${project}-build \
         create route edge \
