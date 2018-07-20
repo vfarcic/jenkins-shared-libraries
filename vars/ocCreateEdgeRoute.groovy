e4d047c5e4d1e503394377eef5528326881ede85
Viktor <viktor@farcic.com>
2018-07-20 23:40:19 +0200

    Jenkins CDP

--- vars/ocCreateEdgeRoute.groovy
+++ vars/ocCreateEdgeRoute.groovy
@@ -1,9 +1,12 @@
 def call(project, address) {
-    try { // Needs to be created only once
+    try {
         sh """oc -n ${project} \
             create route edge \
             --service ${project} \
             --insecure-policy Allow \
             --hostname ${address}"""
-    } catch(e) {}
+    } catch(e) {
+    } finally {
+        echo "Don't panic! The route already exists so it cannot be recreated."
+    }
 }
