f7f52096ee3945c880d1dedc8da3dc74fbd2cbb1
Viktor <viktor@farcic.com>
2018-07-29 14:55:27 +0200

    Jenkinsfile

--- vars/k8sPushHelm.groovy
+++ vars/k8sPushHelm.groovy
@@ -10,7 +10,7 @@ def call(project, chartVersion, museumAddr) {
         passwordVariable: "PASS"
     )]) {
         sh """curl -u $USER:$PASS \
-            --data-binary "@${packageName}" \
+            --data-binary "@helm/${packageName}" \
         http://${museumAddr}/api/charts"""
     }
 }
