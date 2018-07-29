301cedda4a69e44acc3b3caf71b61a2904010b72
Viktor <viktor@farcic.com>
2018-07-29 13:45:55 +0200

    Jenkinsfile

--- vars/k8sPushHelm.groovy
+++ vars/k8sPushHelm.groovy
@@ -1,14 +1,14 @@
 def call(project, chartVersion, museumAddr) {
     sh "helm package helm/${project}"
+    package = "${project}-${chartVersion}.tgz"
+    if (chartVersion == "") {
+        package = sh returnStdout: true, script: "ls project*"
+    }
     withCredentials([usernamePassword(
         credentialsId: "chartmuseum",
         usernameVariable: "USER",
         passwordVariable: "PASS"
     )]) {
-        package = "${project}-${chartVersion}.tgz"
-        if (chartVersion == "") {
-            package = sh returnStdout: true, script: "ls project*"
-        }
         sh """curl -u $USER:$PASS \
             --data-binary "@${package}" \
         http://${museumAddr}/api/charts"""
