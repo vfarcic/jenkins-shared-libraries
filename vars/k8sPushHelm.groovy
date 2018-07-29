55aba2dfa91911fe37b75173a23e9477543773fe
Viktor <viktor@farcic.com>
2018-07-29 21:05:21 +0200

    Helm

--- vars/k8sPushHelm.groovy
+++ vars/k8sPushHelm.groovy
@@ -1,14 +1,14 @@
 def call(project, chartVersion, museumAddr, replaceTag = false) {
-    sh "helm package helm/${project}"
-    packageName = "${project}-${chartVersion}.tgz"
-    if (chartVersion == "") {
-        packageName = sh(returnStdout: true, script: "ls ${project}*").trim()
-    }
     if (replaceTag) {
         yaml = readYaml file: "helm/${project}/values.yaml"
         yaml.image.tag = currentBuild.displayName
         sh "rm -f helm/${project}/values.yaml"
         writeYaml file: "helm/${project}/values.yaml", data: yaml
+    }
+    sh "helm package helm/${project}"
+    packageName = "${project}-${chartVersion}.tgz"
+    if (chartVersion == "") {
+        packageName = sh(returnStdout: true, script: "ls ${project}*").trim()
     }
     withCredentials([usernamePassword(credentialsId: "chartmuseum", usernameVariable: "USER", passwordVariable: "PASS")]) {
         sh """curl -u $USER:$PASS --data-binary "@${packageName}" http://${museumAddr}/api/charts"""
