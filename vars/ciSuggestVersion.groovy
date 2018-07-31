e8549cc02beee232fc7ed1692d7f404ffaf68145
tiko <t.mnatsakanyan@gmail.com>
2018-07-31 11:11:56 +0100

    ci shared groovy

---
+++ vars/ciSuggestVersion.groovy
@@ -0,0 +1,14 @@
+def call(version) {
+    //Give a chance for a custom version
+    try {
+        timeout(time: 15, unit: 'SECONDS') { // change to a convenient timeout for you
+            env.newVersion = input(
+                    id: 'SuggestVersion', message: 'Build auto version ?', parameters: [
+                    [$class: 'StringParameterDefinition',
+                     defaultValue: version,
+                     description: '',
+                     name: 'Confirm release version ']
+            ])
+        }
+    } catch(err) { }
+}
---
+++ vars/ciVersionRead.groovy
@@ -0,0 +1,22 @@
+def call() {
+
+
+    tagBeta = "${currentBuild.displayName}-${env.BRANCH_NAME}"
+
+    prefix = ""
+    if (sudo) {
+        prefix = "sudo "
+    }
+    sh """${prefix}docker image build \
+        -t ${image}:${tagBeta} ."""
+    withCredentials([usernamePassword(
+            credentialsId: "docker",
+            usernameVariable: "USER",
+            passwordVariable: "PASS"
+    )]) {
+        sh """${prefix}docker login \
+            -u $USER -p $PASS"""
+    }
+    sh """${prefix}docker image push \
+        ${image}:${tagBeta}"""
+}
