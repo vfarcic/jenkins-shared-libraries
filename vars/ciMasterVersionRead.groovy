ebb70e254e5a2541e1bc0fae17187e499dc6fe88
tiko <t.mnatsakanyan@gmail.com>
2018-08-08 00:12:11 +0100

    remove git fetch

--- vars/ciMasterVersionRead.groovy
+++ vars/ciMasterVersionRead.groovy
@@ -1,6 +1,4 @@
 def call() {
-    sh "git fetch origin 'refs/tags/*:refs/tags/*'"
     def version = sh(script: 'git tag -l | tail -n1', returnStdout: true).trim() ?: 'v1.0.0'
-
     return version
 }
