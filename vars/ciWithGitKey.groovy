2d6faf94b1134f40323cd03008b2151c9749ad84
tiko <t.mnatsakanyan@gmail.com>
2018-08-10 00:10:41 +0100

    another stupid error

--- vars/ciWithGitKey.groovy
+++ vars/ciWithGitKey.groovy
@@ -1,10 +1,7 @@
 def call(tunk) {
     withCredentials([sshUserPrivateKey(credentialsId: 'TM_RSA', keyFileVariable: 'SSH_KEY')]) {
-
-        if(!exists) {
-            sh 'echo ssh -i $SSH_KEY -l git -o StrictHostKeyChecking=no \\"\\$@\\" > local_ssh.sh'
-            sh 'chmod +x local_ssh.sh'
-        }
+        sh 'echo ssh -i $SSH_KEY -l git -o StrictHostKeyChecking=no \\"\\$@\\" > local_ssh.sh'
+        sh 'chmod +x local_ssh.sh'
 
         withEnv(['GIT_SSH=local_ssh.sh']) {
             tunk()
