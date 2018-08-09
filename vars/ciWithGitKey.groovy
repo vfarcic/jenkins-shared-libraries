be1a2b440928f5a59bf75c28347832ad928d19b9
tiko <t.mnatsakanyan@gmail.com>
2018-08-10 00:07:26 +0100

    again...

--- vars/ciWithGitKey.groovy
+++ vars/ciWithGitKey.groovy
@@ -1,17 +1,12 @@
 def call(tunk) {
     withCredentials([sshUserPrivateKey(credentialsId: 'TM_RSA', keyFileVariable: 'SSH_KEY')]) {
 
-        //for subsequent runs
-        def exists = fileExists 'local_ssh.sh'
-
         if(!exists) {
-            echo "############### file does not exist"
             sh 'echo ssh -i $SSH_KEY -l git -o StrictHostKeyChecking=no \\"\\$@\\" > local_ssh.sh'
             sh 'chmod +x local_ssh.sh'
         }
 
         withEnv(['GIT_SSH=local_ssh.sh']) {
-            echo "############### running tunk "
             tunk()
         }
     }
