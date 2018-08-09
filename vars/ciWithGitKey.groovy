b4419c498de1c4ef91f9dd16bd200c98933bde55
tiko <t.mnatsakanyan@gmail.com>
2018-08-09 20:58:16 +0100

    subsequent runs

--- vars/ciWithGitKey.groovy
+++ vars/ciWithGitKey.groovy
@@ -1,11 +1,14 @@
 def call(tunk) {
     withCredentials([sshUserPrivateKey(credentialsId: 'TM_RSA', keyFileVariable: 'SSH_KEY')]) {
 
-        // on the second run it dies with can't create local_ssh.sh: Permission denied
-        sh 'rm -rf local_ssh.sh'
+        //for subsequent runs
+        def exists = fileExists 'local_ssh.sh'
 
-        sh 'echo ssh -i $SSH_KEY -l git -o StrictHostKeyChecking=no \\"\\$@\\" > local_ssh.sh'
-        sh 'chmod +x local_ssh.sh'
+        if(!exists) {
+            sh 'echo ssh -i $SSH_KEY -l git -o StrictHostKeyChecking=no \\"\\$@\\" > local_ssh.sh'
+            sh 'chmod +x local_ssh.sh'
+        }
+
         withEnv(['GIT_SSH=local_ssh.sh']) {
             tunk()
         }
