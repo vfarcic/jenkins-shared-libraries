c0dfc4d7cfe37a8011a859443de42168c8836423
tiko <t.mnatsakanyan@gmail.com>
2018-08-13 16:58:19 +0100

    factor out key

--- vars/ciMasterVersionRead.groovy
+++ vars/ciMasterVersionRead.groovy
@@ -1,10 +1,10 @@
 def call() {
 
-    ciWithGitKey {
-        sh "git fetch origin 'refs/tags/*:refs/tags/*'"
-        def version = sh(script: 'git tag -l | tail -n1', returnStdout: true).trim() ?: 'v1.0.0'
-        echo "current release is: $version"
-        return version
-    }
+
+    sh "git fetch origin 'refs/tags/*:refs/tags/*'"
+    def version = sh(script: 'git tag -l | tail -n1', returnStdout: true).trim() ?: 'v1.0.0'
+    echo "current release is: $version"
+    return version
+
 
 }
