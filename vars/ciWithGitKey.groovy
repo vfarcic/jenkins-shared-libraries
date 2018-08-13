b4840b3ae227b4f087347dc900d86891bc783450
tiko <t.mnatsakanyan@gmail.com>
2018-08-13 16:56:06 +0100

    credentialId

--- vars/ciWithGitKey.groovy
+++ vars/ciWithGitKey.groovy
@@ -1,5 +1,5 @@
-def call(tunk) {
-    withCredentials([sshUserPrivateKey(credentialsId: 'TM_RSA', keyFileVariable: 'SSH_KEY')]) {
+def call(credentialId, Closure tunk) {
+    withCredentials([sshUserPrivateKey(credentialsId: credentialId, keyFileVariable: 'SSH_KEY')]) {
         sh 'echo ssh -i $SSH_KEY -l git -o StrictHostKeyChecking=no \\"\\$@\\" > local_ssh.sh'
         sh 'chmod +x local_ssh.sh'
 
