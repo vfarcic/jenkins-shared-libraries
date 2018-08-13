76b675bbee104dcf8492106431a9af4da71f2b74
tiko <t.mnatsakanyan@gmail.com>
2018-08-13 17:58:14 +0100

    more escaping

--- vars/ciEscapeBranchName.groovy
+++ vars/ciEscapeBranchName.groovy
@@ -3,6 +3,9 @@ def call() {
             .toString()
             .toLowerCase()
             .replace("/", "-")
+            // we want to use branch name as our chart name, so we try to comply the regexp
+            // (e.g. 'my-name',  or 'abc-123', regex used for validation is '[a-z]([-a-z0-9]*[a-z0-9])?')
+            .replace(".", "-")
 
     return escapedBranch
 }
