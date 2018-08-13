dcfd50933cb6b040028ee1d71ec4095c0cfde99b
tiko <t.mnatsakanyan@gmail.com>
2018-08-13 23:49:09 +0100

    debugs

--- vars/ciDeleteAfterTimingOut.groovy
+++ vars/ciDeleteAfterTimingOut.groovy
@@ -7,11 +7,13 @@ def call(time, unit, Closure tunk) {
     catch (err) {
         def user = err.getCauses()[0].getUser()
         if ('SYSTEM' == user.toString()) { // SYSTEM means timeout.
+            echo "no input was received before timeout"
             container('helm') {
                 ciK8sDeleteBeta(er.project)
             }
             currentBuild.result = 'SUCCESS'
         } else if (env.userInput != null) {
+            echo "input was received from user"
             currentBuild.result = 'SUCCESS'
         } else {
             echo "this was not successful"
