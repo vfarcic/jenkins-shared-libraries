db2b1e060e7f071092a685f8aa5820149b1327ed
tiko <t.mnatsakanyan@gmail.com>
2018-08-14 16:41:40 +0100

    trying explicit namespace

--- vars/ciK8sRolloutBeta.groovy
+++ vars/ciK8sRolloutBeta.groovy
@@ -3,7 +3,7 @@ def call(project) {
     echo " waiting for deployment rollout:  $chartName in namespace: jenkins"
 
     //todo replace jenkins here, its just my env is weird
-    sh """kubectl -n jenkins \
+    sh """kubectl -n ${project}-build \
         rollout status deployment \
         ${chartName.toLowerCase()}"""
 }
