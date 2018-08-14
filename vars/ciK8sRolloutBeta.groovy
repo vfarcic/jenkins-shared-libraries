0aca23ad97f7156563e108f33bbe837b5b330fba
tiko <t.mnatsakanyan@gmail.com>
2018-08-14 17:03:06 +0100

    some logs

--- vars/ciK8sRolloutBeta.groovy
+++ vars/ciK8sRolloutBeta.groovy
@@ -1,8 +1,7 @@
 def call(project) {
     chartName = ciChartNameRead(project)
-    echo " waiting for deployment rollout:  $chartName in namespace: jenkins"
+    echo " waiting for deployment rollout:  $chartName in namespace: ${project}-build"
 
-    //todo replace jenkins here, its just my env is weird
     sh """kubectl -n ${project}-build \
         rollout status deployment \
         ${chartName.toLowerCase()}"""
