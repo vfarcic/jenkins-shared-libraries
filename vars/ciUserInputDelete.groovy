5a471cf24ebe5a1531d80348faae020ec3c9db9c
tiko <t.mnatsakanyan@gmail.com>
2018-08-14 01:13:00 +0100

    debug lines

--- vars/ciUserInputDelete.groovy
+++ vars/ciUserInputDelete.groovy
@@ -1,11 +1,13 @@
 def call() {
     def userInput = input(id: "delete deployment",
             message: "Delete ${env.BRANCH_NAME} deployment?",
-            ok: 'Delete',
+            ok: 'proceed',
             parameters: [[$class: 'BooleanParameterDefinition',  description: '', name: 'Delete']])
 
+    println (" ###############" + userInput )
     if(userInput == true) {
-        ciK8sDeleteBeta(params.project)
+//        ciK8sDeleteBeta(params.project)
+        echo "delete"
     } else {
         echo "[${user}] kept the deployment alive "
     }
