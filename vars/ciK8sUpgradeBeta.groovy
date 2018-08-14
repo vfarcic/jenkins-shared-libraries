d63c4df4565dc02bd8f254506aeb34632f241923
tiko <t.mnatsakanyan@gmail.com>
2018-08-14 16:41:15 +0100

    trying explicit namespace

--- vars/ciK8sUpgradeBeta.groovy
+++ vars/ciK8sUpgradeBeta.groovy
@@ -7,6 +7,7 @@ def call(project, domain, tag, extraValues = "") {
         ${chartName.toLowerCase()} \
         helm/${project} -i \
         --tiller-namespace ${project}-build \
+        --namespace ${project}-build \
         --set image.tag=${tag} \
         --set ingress.host=${addr.toLowerCase()} \
         ${extraValues}"""
