ea4848180322420610f699058b52e5b159ec510a
tiko <t.mnatsakanyan@gmail.com>
2018-12-01 21:18:22 +0000

    bug fixed in jenkins

--- vars/ciBuildEnvVars.groovy
+++ vars/ciBuildEnvVars.groovy
@@ -1,6 +1,4 @@
 def call() {
-    //https://issues.jenkins-ci.org/browse/JENKINS-52623 i found issue and waiting it to be fixed, i think carlos is on it now
-    env.GIT_COMMIT = sh(script: """ git rev-parse HEAD """, returnStdout: true).trim()
     env.shortGitCommit = "${env.GIT_COMMIT[0..10]}"
     env.BUILD_TAG = ciBuildVersionRead()
 
