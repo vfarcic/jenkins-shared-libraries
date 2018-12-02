9ccc8aef5a0ade100c25fe36b16ca0aab5f5ad8c
tiko <t.mnatsakanyan@gmail.com>
2018-12-02 22:45:43 +0000

    sort git tags

--- vars/ciMasterVersionRead.groovy
+++ vars/ciMasterVersionRead.groovy
@@ -1,6 +1,10 @@
 def call() {
+    sh "git config --global tag.sort version:refname"
     sh "git fetch origin 'refs/tags/*:refs/tags/*'"
+
     def version = sh(script: 'git tag -l | tail -n1', returnStdout: true).trim() ?: 'v1.0.0'
+
     echo "current release is: $version"
+
     return version
 }
